3b26bdd6e112d808c2cf1ba8635455f2c04f17dc
sarahisml <sarah.isml22@gmail.com>
2024-05-20 15:33:30 +0800

    final unit

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -18,3 +18,18 @@ const kButtonTextStyle = TextStyle(
 const kConditionTextStyle = TextStyle(
   fontSize: 100.0,
 );
+
+const kTextFieldInputDecoration = InputDecoration(
+  filled: true,
+  fillColor: Colors.white,
+  icon: Icon(Icons.location_city),
+  hintText: 'Enter City Name',
+  hintStyle:
+      TextStyle(color: Colors.grey), //hint text by default is white colour
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(
+      Radius.circular(10.0),
+    ),
+    borderSide: BorderSide.none,
+  ),
+);
